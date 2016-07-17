93460fafd30301561005b8b5545bc9ad23408804
Kameron Johnson <kameronjohnson@gmail.com>
2016-07-17 02:28:59 +0000

    add movies service, update movies & app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { MoviesComponent } from './components/movies/movies.component'; 
+import { MoviesService } from './services/movies.service';
+import { JSONP_PROVIDERS } from '@angular/http';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [MoviesComponent],
+  providers: [MoviesService, JSONP_PROVIDERS]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Daddy!';
 }
---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {Jsonp} from '@angular/http';
+import 'rxjs/Rx';
+
+@Injectable()
+export class MoviesService {
+    apiKey:string;
+    
+    constructor(private _jsonp:Jsonp) {
+        this.apiKey = '30230369ecdfba1b41e74d8ac1cac866';
+        console.log('Daddy movie service running');
+    }
+    
+    getPopular(){
+        return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK&sort_by=popularity.desc&api_key='+this.apiKey)
+                .map(res => res.json());
+    }
+}
